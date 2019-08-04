0f93b8cf4497de7af695b26d026bb99b30068768
karappo-kun <cryogen.vxwizard@gmail.com>
2019-08-04 11:50:16 +0800

    Add PREFER_SAMPLED option

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
 require('dotenv').config();
-// const login = require('./lib/login');
 const getGallery = require('./lib/gallery');
 
 (async () => {
-  // const request = await login();
+  const { GALLERY_URL } = process.env;
 
-  // const galleryUrl = 'https://exhentai.org/g/689418/7fb9d5b310/';
-  // const galleryUrl = 'https://exhentai.org/g/1452116/1d0fc2779a/';
-  // const galleryUrl = 'https://exhentai.org/g/1456825/3db2529829/';
-  // const galleryUrl = 'https://exhentai.org/g/1438739/758825a918/';
-  // const galleryUrl = 'https://exhentai.org/g/700698/aaef5c99f5/';
-  const galleryUrl = 'https://exhentai.org/g/1408701/959c6e9a55/';
-
-  const galleryInfo = await getGallery(galleryUrl);
+  const galleryInfo = await getGallery(GALLERY_URL);
   console.log(galleryInfo);
 })();
